9faed21a661846c29b6b5e5229baf18aa09b26b2
panzer01052005-creator <panzer01052005@gmail.com>
2026-07-01 21:31:33 +0000

    abhishek

---
+++ 01.basic/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "abhishek@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+
+accountEmail = "hc@hd.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+let accountState;
+
+console.log(accountId);
+
+// Prefer not to use var
+// because of issue in block scope and functtional scope.
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
