9a38e3078fb1c002f850f0b2d406e0c339dc761e
Abhishek kumar <panzer01052005@gmail.com>
2026-07-10 20:33:29 +0000

    scope part 1 & 2

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,14 +1,58 @@
 // var c = 300
 let a = 300
 
-if(true) {
-    let a = 10
-    const b = 20
-    console.log(("INNER:", a));
+// if(true) {
+//     let a = 10
+//     const b = 20
+//     console.log(("INNER:", a));
     
-}
+// }
 
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+
+
+function one(){
+    const username = "Abhishek"
+    
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+}
+
+// one()
+
+if(true) {
+    const username = "Abhishek"
+    if (username ==="Abhishek") {
+        const website = " Youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+
+//++++++++++++++++++++++++++++Interesting++++++++++++++++++++
+
+function addone(num){
+    return num + 1
+}
+
+addone(5)
+
+
+const addTwo = function(num) {
+    return num + 2
+}
+
+addTwo(5)
