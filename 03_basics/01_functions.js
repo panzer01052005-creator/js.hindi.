0a4fbd8ed6d0c061b673352779ad22ee87d8088a
Abhishek kumar <panzer01052005@gmail.com>
2026-07-10 18:08:01 +0000

    function part 1&2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -30,15 +30,46 @@ return number1 + number2
  }
 
 const result = addTwoNumbers(34, 435)
-console.log("Result: ", result);
+// console.log("Result: ", result);
 
 function loginUserMessage(username = "sam"){
     if(!username){
-        console.log("Please enter a username");
+        // console.log("Please enter a username");
         return
     }
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("Abhishek"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage("Abhishek"))
+// console.log(loginUserMessage())
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200,400,455,34,34));
+
+
+const user = {
+    username : "Abhishek",
+    price : 200
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 499
+})
+
+const MynewArray = [200, 400, 500, 600]
+
+function returnSecondvalue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondvalue(MynewArray));
+console.log(returnSecondvalue([200,400,400,600,655]));
