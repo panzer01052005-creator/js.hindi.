96ee0c1782ff1091d3f58712223a4bdb038d669d
Abhishek kumar <panzer01052005@gmail.com>
2026-07-14 19:39:17 +0000

    conversion operations

--- 01.basics/03_conversionOperation.js
+++ 01.basics/03_conversionOperation.js
@@ -3,10 +3,13 @@ let score = true
 
 
 // console.log(typeof score);
+
 // console.log(typeof (score));
 
 let valueInNumber = Number(score)
+
 // console.log(typeof valueInNumber);
+
 // console.log(valueInNumber);
 
 // "33" => 33
@@ -27,6 +30,7 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
+
 // console.log(typeof stringNumber);
 
 // ********************Operations ************************
